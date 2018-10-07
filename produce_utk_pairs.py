f096d732dd58d8f47457387d3cf96b5588e26618
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2018-10-06 21:56:41 -0400

    add stats to produce_utk_pairs.py

--- produce_utk_pairs.py
+++ produce_utk_pairs.py
@@ -2,29 +2,47 @@ import argparse
 import os, sys
 import random
 
+RACES = 5
+GENDERS = 2
+
 def main(args):
     print("Generating testing pairs from:", args.utk_dir)
 
     # setup counters for test set metrics
     positive_pairs = 0
     negative_pairs = 0
+    if args.print_stats:
+        race_count = [0 for i in range(RACES)]
+        gender_count = [0 for i in range(GENDERS)]
 
     # fetch subdirectories
     subdirectories = [child[0].split("/")[-1] for child in os.walk(args.utk_dir)][1:]
-    indices = range(len(subdirectories))
+    indices = [i for i in range(len(subdirectories))]
 
     # form pairs of test set
     with open(args.pairs, "w") as file:
         for i, sub in enumerate(subdirectories):
             if i % 1000 == 0: print("{0} samples generated so far.".format(i))
+           
             # sample random negative example       
             j = random.choice(indices[:i] + indices[(i+1):])
             negative_sample = "{0}\t1\t{1}\t1\n".format(sub, subdirectories[j])
             file.write(negative_sample)
-
             negative_pairs = negative_pairs + 1
 
-    # output test set statistics
+            # update class statistics
+            if args.print_stats:
+                sub_split = sub.split("_")
+                gender_count[int(sub_split[1])] = gender_count[int(sub_split[1])] + 1
+                race_count[int(sub_split[2])] = race_count[int(sub_split[2])] + 1
+
+    # output test set class statistics
+    if args.print_stats:
+        print("-------------- Dataset Class Statistics --------------")
+        print("Gender Distribution:", gender_count, "Total:", sum(gender_count))
+        print("Race   Distribution:", race_count, "Total:", sum(race_count))   
+
+    # Output test set statistics
     print("------------------------------------------------------")
     print("Positive:", positive_pairs)
     print("Negative:", negative_pairs)
@@ -37,6 +55,8 @@ def parse_arguments(argv):
         help='Path to the data directory containing UTK identities.')
     parser.add_argument('pairs', type=str,
         help='File to write pairs for test set.')
+    parser.add_argument('--print_stats',
+        help='Prints dataset statistics.', action='store_true')
     return parser.parse_args(argv)
 
 # Sample execution: python produce_utk_pairs.py ../utk_preprocessed/raw/ pairs.txt
